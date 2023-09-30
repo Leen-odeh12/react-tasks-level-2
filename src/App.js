90fb689b0e93678bd0ff363051d0cb0a40942cc9
leen <leenodeh287@gmail.com>
2023-09-30 14:33:38 +0300

    example2 {obj}-useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import UseReducer from "./Component/UseReducer/UseReducer";
+// import UseReducer from "./Component/UseReducer/UseReducer";
+import UseReducer from "./Component/UseReducer/Reducer-Ex-2";
 // import Usestate from "./Component/Usestate/Usestate";
 
 function App() {
   return (
     <div className="App">
       {/* <Usestate /> */}
-      
       <UseReducer/>
+
+      
+
 
     </div>
   );
