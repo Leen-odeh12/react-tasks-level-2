dde1d950a54345c425a23e978ce2c1a4f5b34dcf
leen <leenodeh287@gmail.com>
2023-10-05 22:06:07 +0300

    darkTheme use context hook.

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+
   return (
-    <div style={{display:"flex", justifyContent:"space-around", alignItems:"center"
-    ,backgroundColor:"red"}}>
+    <div
+      style={{ display:"flex"
+      ,justifyContent:"space-around", alignItems:"center"}}
+    >
       <h1>web.dev</h1>
+     
       <nav>
-        <NavLink to="/html" > Html</NavLink>
-        <NavLink to="/css" style={{paddingLeft:"20px",paddingRight:"20px"}}> Css</NavLink>
-        <NavLink to="/js"> Js</NavLink>
+        <NavLink to="/html">Html</NavLink>
+        <NavLink
+          to="/css"
+          style={{ paddingLeft: "20px", paddingRight: "20px" }}
+        >
+          Css
+        </NavLink>
+        <NavLink to="/js">Js</NavLink>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
