3055040fcd309ca509359e63d7ad0b309208365f
leen <leenodeh287@gmail.com>
2023-10-10 13:35:41 +0300

    add soluation useReducer ex-2

--- src/Component/Reducer2/Reducer.jsx
+++ src/Component/Reducer2/Reducer.jsx
@@ -1,45 +1,66 @@
-import React from 'react'
+import React, { useReducer } from "react";
 
-const Reducer = () => {
+const initialState = {
+  name: "",
+  email: "",
+  notes: ""
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "change":
+      return { ...state, [action.field]: action.value };
+
+    case "reset":
+      return initialState;
+
+    default:
+      return state;
+  }
+};
+
+const ReducerComponent = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleInputChange = (field, value) => {
+    dispatch({ type: "change", field, value });
+  };
+
+  const handleReset = () => {
+    dispatch({ type: "reset" });
+  };
+
   return (
     <div>
       <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-      >
-        <div>
-          <label>username: </label>
-          <input
-            type="text"
-           
-          />
-        </div>
-        <div>
-          <label>email: </label>
-          <input
-            type="email"
-           
-          />
-        </div>
-        <div>
-          <label>notes: </label>
-          <input
-            type="text"
-         
-          />
-        </div>
-        <div>
-          <button>submit</button>
-        </div>
-        <div>
-          <button >Reset</button>
-        </div>
-      </form>
+        <label>username : </label>
+        <input
+          type="text"
+          value={state.name}
+          onChange={(e) => handleInputChange("name", e.target.value)}
+        />
+      </div>
+      <div>
+        <label>email : </label>
+        <input
+          type="email"
+          value={state.email}
+          onChange={(e) => handleInputChange("email", e.target.value)}
+        />
+      </div>
+      <div>
+        <label>notes : </label>
+        <input
+          type="text"
+          value={state.notes}
+          onChange={(e) => handleInputChange("notes", e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Reducer
+export default ReducerComponent;
